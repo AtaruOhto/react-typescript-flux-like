7fc248ca7b6911d5745f0fa8897bf3693a6b280b
Ataru Ohto <ataruohto@gmail.com>
2018-10-15 19:23:23 +0900

    add meta info to action

--- src/libs/Action.ts
+++ src/libs/Action.ts
@@ -2,16 +2,19 @@ export class Action<P> {
   private type: string;
   private payload: P;
   private error: boolean;
+  private meta: any;
 
-  constructor(payload: P, error: boolean = false) {
+  constructor(payload: P, error: boolean = false, meta: any = null) {
     this.type = this.constructor.name;
     this.payload = payload;
     this.error = error;
+    this.meta = meta;
   }
 
   public getType = () => this.type;
   public getPayload = () => this.payload;
   public getError = () => this.error;
+  public getMeta = () => this.meta;
 }
 
 export const createAction = <P>(payload: P): Action<P> => new Action(payload);
